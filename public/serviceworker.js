3deefb93e7c4ca6eed0ea833cad37eac9cc11b0b
Hammad-Hasnain <hammadhasnain81@gmail.com>
2025-03-06 14:03:39 +0500

    feat(PWA): implement PWA for this web app and make it installable

---
+++ public/serviceworker.js
@@ -0,0 +1,41 @@
+var CACHE_NAME = "version-1";
+var urlsToCache = ["index.html", "offline.html"];
+
+const self = this;
+// install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened Cache ==>> ", cache);
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+// listen
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(() =>
+        fetch(event.request).catch(() => caches.match("offline.html"))
+      )
+  );
+});
+
+// activate
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
